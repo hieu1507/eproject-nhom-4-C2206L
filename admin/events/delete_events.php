b9d706befa06a112ea891cc93cf4f3388ee06522
trinhdinhvu2312 <dragonking023@gmail.com>
2023-01-09 14:42:23 +0700

    push code

--- admin/events/delete_events.php
+++ admin/events/delete_events.php
@@ -1,15 +1,15 @@
 <?php
 require_once ('../dbhelper.php');
 
-if(isset($_GET['id_ev'])) {
-	$id_ev = $_GET['id_ev'];
+if(isset($_GET['id_events'])) {
+	$id_events = $_GET['id_events'];
 
 	//B1. Mo ket noi toi CSDL
 	$conn = mysqli_connect(HOST, USERNAME, PASSWORD, DB);
 	mysqli_set_charset($conn, 'utf8');
 
 	//B2. Them/sua/xoa/lay du lieu tu database -> insert/update/delete/select
-	$sql = "delete from events where id_ev = $id_ev";
+	$sql = "delete from events where id_events = $id_events";
 	mysqli_query($conn, $sql);
 
 	//B3. Dong ket noi toi CSDL
